db2a3436c3c6a3a83e83e74af64a5ef7a31375d2
huzhihai <huzhihaimail@163.com>
2018-03-18 15:42:31 +0800

    rm out dir

--- out/production/resources/static/vue/common/common-application.js
+++
@@ -1,7 +0,0 @@
-// 全局配置定义
-const APP_NAME = "/triceps"; // 应用名称
-const VUE_EL = "app"; // vue的el属性统一名称
-const TABLE_ID = "grid"; // 显示表格的ID
-const PAGE_NUMBER = 1; // 表格起始页码
-const TABLE_PAGE_SIZE = 10; // 表格每页大小
-const TABLE_PAGE_SIZE_LIST = [10, 25, 50, 100]; // 设置表格每页显示多少条
